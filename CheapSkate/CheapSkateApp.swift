4f57b7aa73d6a9492359fcad17d39bb8bfa6c487
Brendyn Dabrowski <bdabrowski@live.com>
2022-12-28 21:36:09 -0800

    Initial Commit

---
+++ CheapSkate/CheapSkateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheapSkateApp.swift
+//  CheapSkate
+//
+//  Created by Brendyn Dabrowski on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct CheapSkateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
