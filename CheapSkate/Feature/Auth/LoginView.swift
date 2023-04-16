bfd8d79bc6c1e4cbc9e8273c4a26bbcb8647dd0b
Brendyn Dabrowski <bdabrowski@live.com>
2023-04-16 15:27:43 -0500

    Removed unneeded ViewState from LoginView.

--- CheapSkate/Feature/Auth/LoginView.swift
+++ CheapSkate/Feature/Auth/LoginView.swift
@@ -10,17 +10,13 @@ import ComposableArchitecture
 
 struct LoginView: View {
     @State private var username: String = ""
-    @State private var password: String = "" // see if we can put these in viewState below.
-    let store: StoreOf<Login>
-    @ObservedObject var viewStore: ViewStore<ViewState, Login.Action>
-    
-    struct ViewState: Equatable {
-        init(state: Login.State) { }
-    }
+    @State private var password: String = ""
+    private let store: StoreOf<Login>
+    @ObservedObject private var viewStore: ViewStore<Void, Login.Action>
     
     init(store: StoreOf<Login>) {
         self.store = store
-        self.viewStore = ViewStore(self.store.scope(state: ViewState.init))
+        self.viewStore = ViewStore(self.store.stateless)
     }
     
     var body: some View {
@@ -30,8 +26,10 @@ struct LoginView: View {
             
             VStack {
                 TextField("Username", text: $username)
+                    .disableAutocorrection(true)
                     .textInputAutocapitalization(TextInputAutocapitalization.never)
                 SecureField("Password", text: $password)
+                    .disableAutocorrection(true)
                     .textInputAutocapitalization(TextInputAutocapitalization.never)
                 Button("Login", action: {
                     viewStore.send(.submitLogin(username, password))
