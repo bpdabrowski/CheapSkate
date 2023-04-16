52f50e5835e3b0185845fd056940fe1752e29010
Brendyn Dabrowski <bdabrowski@live.com>
2023-04-15 19:19:55 -0500

    migrated ExpenseFeature to ReducerProtocol.

--- CheapSkate/Feature/Expense/ExpenseFeature.swift
+++ CheapSkate/Feature/Expense/ExpenseFeature.swift
@@ -24,98 +24,95 @@ struct ExpenseData: Equatable, Codable {
     var date: Date = Date()
 }
 
-struct ExpenseState: Equatable {
-    var data: ExpenseData = ExpenseData()
-    var viewState: ExpenseViewState = .idle
-    var chartData: [ExpenseData] = []
-}
-
-enum ExpenseAction: BindableAction {
-    case binding(BindingAction<ExpenseState>)
-    case onAppear
-    case selectCategory(ExpenseCategory)
-    case submitExpense
-    case handleSubmitResult(Result<Void, APIError>)
-    case resetState
-    case getExpenses(Date? = nil)
-    case handleGetExpenseResult(Result<[ExpenseData], APIError>)
-    case submitLogin(String, String)
-    case handleLoginResult(Result<AuthResult, APIError>)
-    case showLogoutView // I think this should be moved into its own state and then scoped in the proper context.
-}
-
-struct ExpenseEnvironment {
-    var saveExpense: (ExpenseData) -> Effect<Void, APIError>
-    var getExpenses: (Date?) -> Effect<[ExpenseData], APIError>
-}
-
-let expenseReducer = Reducer<
-  ExpenseState,
-  ExpenseAction,
-  SystemEnvironment<ExpenseEnvironment>
-> { state, action, environment in
-    switch action {
-    case .binding:
-        return .none
-    case .onAppear:
-        if Auth().token == nil {
-            return Effect(value: .showLogoutView)
-        } else {
-            return Effect(value: .getExpenses(Date()))
-        }
-    case .selectCategory(let category):
-        state.data.category = category
-        return .none
-    case .submitExpense:
-        state.data.date = Date()
-        state.viewState = .submitInProgress
-        return environment.saveExpense(state.data)
-            .receive(on: environment.mainQueue())
-            .catchToEffect()
-            .map(ExpenseAction.handleSubmitResult)
-    case .handleSubmitResult(let result):
-        switch result {
-        case .success:
-            state.viewState = .submitSuccessful
-        case .failure:
-            state.viewState = .submitError
-        }
-        return .none
-    case .resetState:
-        state.data.category = .food
-        state.data.amount = 0.00
-        state.viewState = .idle
-
-        return Effect(value: .getExpenses(Date()))
-    case .getExpenses(let date):
-        return environment.getExpenses(date)
-            .receive(on: environment.mainQueue())
-            .catchToEffect()
-            .map(ExpenseAction.handleGetExpenseResult)
-    case .handleGetExpenseResult(let result):
-        switch result {
-        case .success(let chartData):
-            state.chartData = chartData.sorted(by: { $0.category.rawValue < $1.category.rawValue })
-        case .failure:
-            break
-        }
-        return .none
-    case .submitLogin(let username, let password):
-        return Auth().login(username: username, password: password) // probably need to put auth in an environment class.
-            .receive(on: environment.mainQueue())
-            .catchToEffect()
-            .map(ExpenseAction.handleLoginResult)
-    case .handleLoginResult(let result):
-        switch result {
-        case .success:
-            state.viewState = .idle
-        case .failure:
-            state.viewState = .logout
-        }
-        return Effect(value: .getExpenses(Date()))
-    case .showLogoutView:
-        Auth().logout()
-        state.viewState = .logout
-        return .none
+struct ExpenseFeature: ReducerProtocol {
+    struct State: Equatable {
+        var data: ExpenseData = ExpenseData()
+        var viewState: ExpenseViewState = .idle
+        var chartData: [ExpenseData] = []
     }
-}.binding()
+    
+    enum Action: BindableAction {
+        case binding(BindingAction<State>)
+        case onAppear
+        case selectCategory(ExpenseCategory)
+        case submitExpense
+        case handleSubmitResult(Result<Void, APIError>)
+        case resetState
+        case getExpenses(Date? = nil)
+        case handleGetExpenseResult(Result<[ExpenseData], APIError>)
+        case submitLogin(String, String)
+        case handleLoginResult(Result<AuthResult, APIError>)
+        case showLogoutView // I think this should be moved into its own state and then scoped in the proper context.
+    }
+    
+    var saveExpense: (ExpenseData) -> EffectPublisher<Void, APIError>
+    var getExpenses: (Date?) -> EffectPublisher<[ExpenseData], APIError>
+    @Dependency(\.mainQueue) var mainQueue
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .binding:
+            return .none
+        case .onAppear:
+            if Auth().token == nil {
+                return Effect(value: .showLogoutView)
+            } else {
+                return Effect(value: .getExpenses(Date()))
+            }
+        case .selectCategory(let category):
+            state.data.category = category
+            return .none
+        case .submitExpense:
+            state.data.date = Date()
+            state.viewState = .submitInProgress
+            return saveExpense(state.data)
+                .receive(on: mainQueue)
+                .catchToEffect()
+                .map(ExpenseFeature.Action.handleSubmitResult)
+        case .handleSubmitResult(let result):
+            switch result {
+            case .success:
+                state.viewState = .submitSuccessful
+            case .failure:
+                state.viewState = .submitError
+            }
+            return .none
+        case .resetState:
+            state.data.category = .food
+            state.data.amount = 0.00
+            state.viewState = .idle
+            
+            return Effect(value: .getExpenses(Date()))
+        case .getExpenses(let date):
+            return getExpenses(date)
+                .receive(on: mainQueue)
+                .catchToEffect()
+                .map(ExpenseFeature.Action.handleGetExpenseResult)
+        case .handleGetExpenseResult(let result):
+            switch result {
+            case .success(let chartData):
+                state.chartData = chartData.sorted(by: { $0.category.rawValue < $1.category.rawValue })
+            case .failure:
+                break
+            }
+            return .none
+        case .submitLogin(let username, let password):
+            return Auth().login(username: username, password: password) // probably need to put auth in an environment class.
+                .receive(on: mainQueue)
+                .catchToEffect()
+                .map(ExpenseFeature.Action.handleLoginResult)
+        case .handleLoginResult(let result):
+            switch result {
+            case .success:
+                state.viewState = .idle
+            case .failure:
+                state.viewState = .logout
+            }
+            return Effect(value: .getExpenses(Date()))
+        case .showLogoutView:
+            Auth().logout()
+            state.viewState = .logout
+            return .none
+        }
+    }
+}
